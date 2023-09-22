ef7ca3942a18f9d23cb8d4692f5c520b3ab668e5
duke <dk812821001@163.com>
2023-09-22 18:58:46 +0800

    Feat: 新增增强测试

---
+++ test_augment.py
@@ -0,0 +1,37 @@
+from utils import create_AugTransforms
+from PIL import Image
+import numpy as np
+
+
+augs = {
+    'random_color_jitter': dict(brightness=0.5, contrast = 0.5, saturation = 0.5, hue = 0.5),
+    'random_cutout': dict(n_holes = 4, length = 50, prob=0.1, ) # dict(n_holes = 1, length = 100, ratio = 0.2, prob = 0.5)
+}
+
+t = create_AugTransforms(augs)
+# t = create_AugTransforms('random_color_jitter random_cutout')
+
+img = Image.open('/Users/duke/project/dataWork/DATA/train/B-/112221101-6_1366972_28302999.jpg')
+
+images = []
+for i in range(1, 29):
+    filename = f'image_{i}.jpg'
+    image = t(img)
+    images.append(image)
+
+# 将图像列表转换为 numpy 数组
+array_images = [np.array(image) for image in images]
+
+# 创建一个 4 行 5 列的图像网格
+rows, cols = 4, 7
+grid = np.zeros((rows * array_images[0].shape[0], cols * array_images[0].shape[1], 3), dtype=np.uint8)
+
+# 在图像网格中排列图像
+for r in range(rows):
+    for c in range(cols):
+        image_index = r * cols + c
+        if image_index < len(array_images):
+            grid[r*array_images[0].shape[0]:(r+1)*array_images[0].shape[0], c*array_images[0].shape[1]:(c+1)*array_images[0].shape[1], :] = array_images[image_index]
+
+# 将图像网格转换为 PIL.Image 对象并显示
+Image.fromarray(grid).show()
